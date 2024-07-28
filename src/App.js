4848d4d4cb078e867b71839807d3bd16084c591f
LAPTOP-LS3RQA28\walru <walrus38@icloud.com>
2024-07-28 18:24:56 -0400

    Added a new tab with a react-data-grid displaying all zipcodes, with the same columns as the outlier table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import 'react-data-grid/lib/styles.css';
 import { WebR } from 'webr';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
+import DataGrid from 'react-data-grid';
 
 const webR = new WebR({repoUrl: "https://repo.r-wasm.org/"});
 await webR.init();
@@ -18,7 +20,7 @@ const categoryNames = await webR.evalRRaw(
   names(dataCT) <- lapply(dataCT[1,], as.character)
   dataCT <- dataCT[-1,]
   dataCT[,3:323] <- sapply(dataCT[,3:323], as.numeric)
-  selectDataCT <- dataCT[,c(1,2,3,75,183,186,187,211,248:253,255,256,276:278,285,286,295:299,302,303,306:308,310,311,321,322)]
+  selectDataCT <- dataCT[,c(1,2,3,75,183,186,248:253,255,256,276:278,285,286,295:299,302,303,306:308,310,311,321,322)]
   names(selectDataCT) <- catNames[,1]
   print(names(selectDataCT))`,
 'string[]'
@@ -31,7 +33,7 @@ async function getAPlot(x,y,labelO,listO) {
       return(x < quantile(x, 0.25, na.rm = T) - 2 * IQR(x, na.rm = T) | x > quantile(x, 0.75, na.rm = T) + 2 * IQR(x, na.rm = T))
     }
     
-    dfToUse <- data.frame(x = selectDataCT$\`${x}\`, y = selectDataCT$\`${y}\`)
+    dfToUse <- data.frame(x = selectDataCT$\`${x}\`, y = selectDataCT$\`${y}\`, pop = selectDataCT$\`Total Population\`)
     model <- lm(dfToUse$x~dfToUse$y, na.action = "na.exclude")
 
     fits <- model$coefficients[1] + (model$coefficients[2] * dfToUse$x)
@@ -62,18 +64,40 @@ async function getAPlot(x,y,labelO,listO) {
       tempStrings <- paste(names(tempList), unlist(tempList), sep = ": ")
       tempString <- paste(tempStrings, collapse = ", ")
       rowStrings <- c(rowStrings, tempString)
+
+      dfToUse$zipcode <- str_sub(selectDataCT$\`Geographic Area Name\`, start = -5)
+    dfToUse$city <- NA
+    for (z in dfToUse$zipcode) {
+      dfToUse[dfToUse$zipcode == z,]$city <- zipTable[zipTable$zip == as.integer(z),]$city
+    }
+    
+    allRowStrings <- c()
+    for(r in 1:(nrow(dfToUse))) {
+      tempList <- as.list(dfToUse[r,])
+      tempStrings <- paste(names(tempList), unlist(tempList), sep = ": ")
+      tempString <- paste(tempStrings, collapse = ", ")
+      allRowStrings <- c(allRowStrings, tempString)
+    }
     }`, {
     captureGraphics: {
       width: 600,
       height: 300
     }
 });
-  return capture
+  return(capture)
 }
 
 async function getOutlierData() {
   const result = await webR.evalRRaw(
     'print(rowStrings)',
+    'string[]'
+  );
+  return(result)
+}
+
+async function getAllRows() {
+  const result = await webR.evalRRaw(
+    `print(allRowStrings)`,
     'string[]'
   );
   return(result)
@@ -88,25 +112,69 @@ function App() {
   const [picture, setPicture] = useState()
   const [outliers, setOutliers] = useState()
   const [loading, setLoading] = useState(false)
+  const [rows, setRows] = useState([])
+  const [xColName, setXColName] = useState(null)
+  const [yColName, setYColName] = useState(null)
+
+  const initialSort = [{ columnKey: 'zipcode', direction: 'ASC' }];
+  const [sortColumns, setSortColumns] = useState(initialSort)
+  const sortedRows = useMemo(() => {
+    const sortedRows = [...rows];
+    sortedRows.sort((a,b) => {
+      for (const sort of sortColumns) {
+        const compResult = a[sort.columnKey] - b[sort.columnKey];
+        if (compResult !== 0) {
+          return sort.direction === "ASC" ? compResult : -compResult;
+        }
+      }
+      return 0;
+    });
+    return sortedRows;
+  }, [rows, sortColumns]);
+
+  function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
 
   function getResults() {
+    var xName = xAxis
+    var yName = yAxis
     setLoading(true);
     getAPlot(xAxis, yAxis, labelOutliers, listOutliers).then(r => {
       setPicture(r.images);
+      setXColName(xName);
+      setYColName(yName);
       if (listOutliers) {
         getOutlierData().then(res => {
-          var outs = tidyOutliers(res);
+          var outs = tidyRows(res);
           setOutliers(outs);
           setShowOutlierTable(true)
         })
       } else {
         setShowOutlierTable(false)
       };
+      getAllRows().then(r => {
+        var theRows = tidyRows(r);
+        setRows(theRows);
+      });
       setLoading(false)
-  });
+    });
   }
 
-  function tidyOutliers(r) {
+  function tidyRows(r) {
     var tidied = r.map(str => {
       var parsed = {}
       var pairs = str.split(", ")
@@ -131,6 +199,7 @@ function App() {
               <th scope="col">City</th>
               <th scope="col">{xAxis}</th>
               <th scope="col">{yAxis}</th>
+              <th scope="col">Total Population</th>
             </tr>
             {outliers.map(row =>
               <tr>
@@ -140,6 +209,7 @@ function App() {
                 <td>{row.city}</td>
                 <td>{row.x}</td>
                 <td>{row.y}</td>
+                <td>{row.pop}</td>
               </tr>
             )}
           </tbody>
@@ -149,6 +219,44 @@ function App() {
       return null
     }
   }
+
+  function AllRowsTable() {
+    if (xColName && yColName) {
+      return (
+        <DataGrid
+          columns={columns}
+          rows={sortedRows}
+          onRowsChange={setRows}
+          sortColumns={sortColumns}
+          onSortColumnsChange={onSortColumnsChange}
+          defaultColumnOptions={{
+            sortable: true,
+            resizable: true
+          }}
+          columnAutoWidth
+        />
+      )
+    } else {
+      return "Get a plot first"
+    }
+    
+  }
+
+  function onSortColumnsChange(newSortColumns) {
+    if (newSortColumns.length === 0) {
+      setSortColumns(initialSort);
+    } else {
+      setSortColumns(newSortColumns);
+    }
+  }
+
+  const columns = [
+    {key: "zipcode", name: "Zipcode"},
+    {key: "city", name: "City", sortable: false},
+    {key: "x", name: xColName},
+    {key: "y", name: yColName},
+    {key: "pop", name: "Total Population"}
+  ]
 
   useEffect(() => {
     if (picture) {
@@ -193,7 +301,18 @@ function App() {
             Get a plot!
           </button>
         </p>
-        <canvas id="plot-canvas" width="900" height="450" style={{border: "solid white"}}/>
+        <p style={{width: "80%"}}>
+          <div class="tab">
+            <button class="tablinks" onClick={e => openTab(e, 'Plot')}>Plot</button>
+            <button class="tablinks" onClick={e => openTab(e, 'Table')}>All Zipcodes</button>
+          </div>
+          <div id="Plot" class="tabcontent">
+            <canvas id="plot-canvas" width="900" height="450" style={{border: "solid white"}}/>
+          </div>
+          <div id="Table" class="tabcontent">
+            <AllRowsTable/>
+          </div>
+        </p>
         <br/>
         <OutlierTable/>
       </header>
